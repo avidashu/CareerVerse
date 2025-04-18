7f26f575aeb8717407a789183d8075ad227d288b
Ashutosh <ashuisavid@gmail.com>
2025-04-18 11:24:26 +0530

    First commit

---
+++ careerverse/src/main/java/com/ashucodes/careerverse/CareerverseApplication.java
@@ -0,0 +1,13 @@
+package com.ashucodes.careerverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerverseApplication.class, args);
+	}
+
+}
---
+++ careerverse/src/test/java/com/ashucodes/careerverse/CareerverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashucodes.careerverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
