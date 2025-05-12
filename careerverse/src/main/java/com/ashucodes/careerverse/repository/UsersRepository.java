89ed992237d4aa17c4f5966f026d23784f202d88
Ashutosh <ashuisavid@gmail.com>
2025-05-12 10:44:51 +0530

    bug fix : duplicate email

--- careerverse/src/main/java/com/ashucodes/careerverse/repository/UsersRepository.java
+++ careerverse/src/main/java/com/ashucodes/careerverse/repository/UsersRepository.java
@@ -3,7 +3,10 @@ package com.ashucodes.careerverse.repository;
 import com.ashucodes.careerverse.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UsersRepository extends JpaRepository<Users,Integer>{
 
+    Optional<Users> findByEmail(String email);
 }
